7515f44777d7bb75b0455346f9ac423a1b280231
Febri Silaen <Febrisialen3103@gmail.com>
2025-04-15 10:48:02 +0700

    update ulos kita & css

--- resources/views/users/uloskita.blade.php
+++ resources/views/users/uloskita.blade.php
@@ -1,34 +1,48 @@
 @extends('layouts.User')
 
 @section('content')
-<div class="container mt-4">
-    <h2 class="mb-4">Jenis-jenis Ulos Kita</h2>
+<div class="container-fluid p-0">
+    <!-- Hero Section -->
+    <div class="text-center hero-section">
+        <div class="hero-overlay">
+            <h1 class="display-4 mb-4">Menghidupkan Kembali Pesona Ulos Batak dengan Sentuhan Keahlian Turun-temurun</h1>
+        </div>
+    </div>
     
-    <div class="row">
-        <!-- Looping through each type of ulos -->
-        @foreach($ulosData as $ulos)
-        <div class="col-md-4 mb-4">
-            <div class="card">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-4">
-                            <img src="{{ asset('img/ulos/' . $ulos['image']) }}" alt="{{ $ulos['name'] }}" class="img-fluid">
+    <!-- Subtitle Section -->
+    <div class="container-fluid text-center subtitle-section">
+        <h2 class="mb-0">Jenis Jenis Ulos Dan Kegunaannya</h2>
+    </div>
+    
+    <!-- Cards Section -->
+    <div class="container">
+        <div class="row">
+            @foreach($ulosData as $ulos)
+            <div class="col-md-6 mb-4">
+                <div class="card ulos-card">
+                    <div class="row no-gutters">
+                        <div class="col-md-5 ulos-image-container">
+                            <img src="{{ asset('img/ulos/' . $ulos['image']) }}" alt="{{ $ulos['name'] }}" class="img-fluid ulos-image">
                         </div>
-                        <div class="col-8">
-                            <!-- Make the ulos name clickable -->
-                            <h4>
-                                <a href="{{ route('uloskita.detail', ['jenis' => $ulos['slug']]) }}" class="text-primary text-decoration-none">
-                                    {{ $ulos['name'] }}
+                        <div class="col-md-7">
+                            <div class="card-body ulos-content">
+                                <div class="ulos-title-container">
+                                    <h4 class="ulos-title">
+                                        <span class="ulos-title-text">{{ $ulos['name'] }}</span>
+                                    </h4>
+                                    
+                                </div>
+                                <p class="ulos-description">{{ Str::limit($ulos['short_description'], 100) }}</p>
+                                <a href="{{ route('uloskita.detail', ['jenis' => $ulos['slug']]) }}" class="ulos-explore-btn">
+                                    Lihat Detail Ulos
                                 </a>
-                            </h4>
-                            <!-- Short description preview -->
-                            <p class="text-muted small">{{ Str::limit($ulos['short_description'], 100) }}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+            @endforeach
         </div>
-        @endforeach
     </div>
 </div>
 @endsection
