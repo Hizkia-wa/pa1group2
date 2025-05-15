a8d815b5f244c720f14b4690c9ba5c8578a01fa8
Febri Silaen <Febrisialen3103@gmail.com>
2025-05-15 08:34:14 +0700

    update

--- app/Http/Controllers/users/UloskitaController.php
+++ app/Http/Controllers/users/UloskitaController.php
@@ -45,15 +45,67 @@ class UloskitaController extends Controller
        ],
 
             [
-                'name' => 'Ulos Sadum',
+                'name' => 'Ulos Sarum Tarutung',
                 'slug' => 'sadum',
                 'category' => 'ulos',
                 'short_description' => 'Ulos Sadum dikenal dengan warna-warnanya yang cerah dan meriah.',
-                'image' => 'ulossadum.jpg',
+                'image' => 'ulossarumtarutung.jpg',
                 'description' => 'Ulos Sadum memiliki ciri khas warna-warna cerah seperti merah, kuning, dan hijau dengan motif yang lebih bebas dan meriah dibandingkan ulos lainnya. Kain ini sering dihiasi dengan benang emas atau perak.',
                 'kegunaan' => 'Ulos Sadum digunakan dalam acara-acara adat yang penuh sukacita, seperti pesta pernikahan atau kelahiran, dan sering diberikan sebagai hadiah kepada tamu penting.',
                 'pembuatan' => 'Pembuatan Ulos Sadum membutuhkan keterampilan khusus untuk menggabungkan warna-warna cerah dan benang logam, dengan proses tenun yang cermat.'
             ],
+                [
+            'name' => 'Ulos Ragi Hotang',
+            'slug' => 'ragi-hotang',
+            'category' => 'ulos',
+            'short_description' => 'Ulos Ragi Hotang adalah ulos yang melambangkan ikatan kasih sayang yang kuat, seperti rotan yang mengikat erat.',
+            'image' => 'ulosragihotang.jpg',
+            'description' => 'Ulos Ragi Hotang merupakan salah satu ulos khas Batak Toba yang digunakan dalam acara pesta adat pernikahan dan kematian. Ulos ini melambangkan kasih sayang, restu, dan harapan agar ikatan antara pengantin atau keluarga tetap kuat dan langgeng seperti ikatan rotan (hotang). Motif dan pewarnaan ulos ini memiliki makna filosofis mendalam yang berkaitan dengan nilai-nilai sosial dan religi masyarakat Batak.',
+            'kegunaan' => 'Digunakan dalam upacara pernikahan adat Batak Toba, khususnya diberikan kepada pengantin sebagai simbol restu dan doa agar pernikahan mereka diberkati dan langgeng. Selain itu, ulos ini juga dipakai dalam acara kematian sebagai tanda penghormatan dan ikatan sosial.',
+            'pembuatan' => 'Pembuatan Ulos Ragi Hotang dilakukan dengan teknik tenun tradisional yang memerlukan ketelitian tinggi. Motif-motifnya dirancang secara khusus untuk melambangkan nilai-nilai budaya dan filosofi Batak, dengan pewarnaan alami dan proses yang memakan waktu lama agar menghasilkan kain yang berkualitas dan bermakna.'
+                ],
+
+
+                [
+                'name' => 'Ulos Suri-suri Ganjang',
+                'slug' => 'suri-suri-ganjang',
+                'category' => 'ulos',
+                'short_description' => 'Ulos Suri-suri Ganjang adalah ulos yang memiliki motif seperti sisir memanjang dan berukuran lebih panjang dari ulos biasa.',
+                'image' => 'ulossurisuriganjang.jpg',
+                'description' => 'Ulos Suri-suri Ganjang merupakan ulos khas Batak Toba yang memiliki ciri khas raginya berbentuk sisir memanjang dan ukuran yang lebih panjang dibanding ulos lain. Ulos ini biasanya digunakan sebagai ulos hela dalam upacara pernikahan dan juga dipakai oleh pihak hula-hula saat margondang untuk memberkati pengantin perempuan. Ulos ini melambangkan berkat, dukungan, cinta, dan doa yang diwariskan turun-temurun dalam keluarga Batak.',
+                'kegunaan' => 'Digunakan sebagai ulos hela dalam pesta pernikahan, serta sebagai ulos berkat oleh orang tua pihak istri saat margondang (memukul gendang) untuk mendoakan pengantin perempuan. Ulos ini juga dapat dipakai dengan cara dililit dua kali pada bahu kiri dan kanan sehingga tampak seperti memakai dua ulos sekaligus.',
+                'pembuatan' => 'Pembuatan ulos ini memerlukan ketelitian tinggi dengan teknik tenun tradisional khas Batak, menghasilkan motif garis lurus sebanyak 33 garis yang melambangkan ciri khas orang Batak yang teguh dan diwariskan turun-temurun.'
+            ]
+            ,
+
+
+
+            [
+            'name' => 'Ulos Ragi Huting',
+            'slug' => 'ragi-huting',
+            'category' => 'ulos',
+            'short_description' => 'Ulos Ragi Huting adalah kain ulos tradisional Batak Toba yang dipakai oleh gadis perawan sebagai simbol status dan identitas budaya.',
+            'image' => 'ulosragihuting.jpg',
+            'description' => 'Ulos Ragi Huting merupakan ulos khas Batak Toba yang dulu sering dipakai oleh gadis-gadis Batak sebagai pakaian sehari-hari, dililitkan di dada (hobahoba). Ulos ini melambangkan status seorang gadis perawan yang beradat serta memiliki nilai budaya dan simbolisme yang kuat dalam masyarakat Batak. Motif dan warna ulos ini khas dengan dominasi warna coklat dan hitam yang dibuat dengan teknik tenun tangan tradisional.',
+            'kegunaan' => 'Biasanya dipakai oleh gadis Batak sebagai penanda status perawan dalam adat Batak Toba. Ulos ini juga digunakan dalam berbagai upacara adat yang melibatkan kaum perempuan, menegaskan identitas dan nilai sosial budaya Batak.',
+            'pembuatan' => 'Pembuatan Ulos Ragi Huting dilakukan dengan teknik tenun tangan yang memerlukan ketelitian dan keahlian khusus. Pewarnaan menggunakan bahan alami dan proses tradisional yang diwariskan secara turun-temurun oleh pengrajin Batak.'
+            ],
+
+           [
+            'name' => 'Ulos Sibolang',
+            'slug' => 'dibollang',
+            'category' => 'ulos',
+            'short_description' => 'Ulos Sibolang adalah kain ulos khas Batak Toba yang memiliki motif seperti ombak, melambangkan tantangan hidup dan kekuatan pemakainya.',
+            'image' => 'Ulos Sibolang.jpg',
+            'description' => 'Ulos Sibolang merupakan salah satu ulos yang paling banyak digunakan dalam adat Batak Toba. Motifnya menyerupai ombak yang menggambarkan tantangan hidup sehingga mendorong pemakainya untuk mampu melewati berbagai rintangan. Ulos ini digunakan dalam berbagai acara adat, baik sukacita maupun duka cita, sehingga sangat familiar dan serbaguna dalam masyarakat Batak.',
+            'kegunaan' => 'Ulos Sibolang dipakai dalam acara adat seperti pesta pernikahan, kelahiran, maupun upacara duka cita. Dalam acara duka, ulos ini biasanya berwarna dominan hitam dan digunakan sebagai ulos saput atau ulos tujung. Sedangkan dalam acara sukacita, ulos ini sering berwarna putih dan digunakan sebagai penutup atau selendang.',
+            'pembuatan' => 'Pembuatan Ulos Sibolang menggunakan teknik tenun tradisional gedogan dengan bahan katun dan pewarna sintetis. Proses tenun dilakukan secara manual oleh para pengrajin yang mewarisi keterampilan ini secara turun-temurun.'
+           ],
+
+
+
+            
+            
             
         ];
     }
