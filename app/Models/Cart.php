db7b19ea0016c8ffcbf4ae365ac0640d3348f408
Hizkia-wa <siahaanhizkia06@gmail.com>
2025-05-15 16:25:55 +0700

    keranjang

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -20,6 +20,6 @@ class Cart extends Model
 
     public function product()
     {
-        return $this->belongsTo(Product::class, 'ProductId');
+        return $this->belongsTo(product::class, 'ProductId');
     }
 }
